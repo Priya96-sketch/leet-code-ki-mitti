0a4a6f84835fe4dae98173451d28a167610e9573
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2021-01-08 19:25:03 +0530

    Create otherSol.cpp

---
+++ Squares-of-sorted-arr/otherSol.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        for(auto& i : A){
+            i *= i;
+        }
+        
+        sort(A.begin(), A.end());
+        
+        return A;
+    }
+};
